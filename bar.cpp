b9f83032ddbb999c3faf9639d416a5b9f7884131
Smart <19011519-045@uog.edu.pk>
2020-07-26 13:57:46 +0500

    updated

---
+++ bar.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class bar
+{
+private:
+
+    
+public:
+
+};
+
+
+
+int main(){
+
+
+    return(0);
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 using namespace std;
+
 int main(){
 
-    cout<<"brick is here";
+    
     
     return(0);
 }
